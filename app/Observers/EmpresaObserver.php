2692e20d2aa2d6a5a52820eae05475feb42d3b24
kevin <kriverarojas44@gmail.com>
2026-07-07 00:02:15 -0500

    ajuste

--- app/Observers/EmpresaObserver.php
+++ app/Observers/EmpresaObserver.php
@@ -30,7 +30,7 @@ class EmpresaObserver
             (new ClienteGeneralSeeder())->runForEmpresa($empresa);
             (new SeriesSeeder())->runForEmpresa($empresa);
             (new RolesEmpresaSeeder())->runForEmpresa($empresa);
-            (new ProductosPruebaSeeder())->runForEmpresa($empresa);
+            // (new ProductosPruebaSeeder())->runForEmpresa($empresa);
         } finally {
             app()->forgetInstance('bypass_tenant_scope');
         }
